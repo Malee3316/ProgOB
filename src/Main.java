d8abaf53375a18fe5b553cd5b5b749d71863e7d8
Malena3316 <Malenamurcia1235@gmail.com>
2024-04-25 13:21:42 -0300

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String nombre1 = "Malena";
+        String nombre2 = "Murcia";
+
+        if (nombre1.equals(nombre2)) {
+            System.out.println("Los nombres son iguales ");
+        } else {
+            System.out.println("Los nombres son diferentes");
+        }
+    }
+}
